2b36996ef85178c209c3e1a3de21e079c3909d62
Ildar <45717260+Karantir73@users.noreply.github.com>
2021-12-12 12:26:04 +0300

    split app in two classes

---
+++ crypto/Crypto.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace crypto
+{
+    class Crypto
+    {
+        private RSACryptoServiceProvider rSA = new RSACryptoServiceProvider();
+        private MD5CryptoServiceProvider mD5 = new MD5CryptoServiceProvider();
+
+        public MD5CryptoServiceProvider MD5 { get => mD5; set => mD5 = value; }
+        public RSACryptoServiceProvider RSA { get => rSA; set => rSA = value; }
+
+        public byte[] RSAencryption(byte[] data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData = { 0 };
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public byte[] RSAdecryption(byte[] data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData = { 0 };
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        public byte[] MD5encryption(byte[] data, char[] privateKey)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(privateKey));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateEncryptor())
+                    {
+                        return transform.TransformFinalBlock(data, 0, data.Length);
+                    }
+                }
+            }
+        }
+
+        public byte[] MD5decryption(byte[] data, char[] privateKey)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(privateKey));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateDecryptor())
+                    {
+                        return transform.TransformFinalBlock(data, 0, data.Length);
+                    }
+                }
+            }
+        }
+    }
+}
--- crypto/Form.cs
+++ crypto/Form.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Security.Cryptography;
 
 namespace crypto
 {
@@ -17,88 +10,15 @@ namespace crypto
         {
             InitializeComponent();
             Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            MinimumSize = new Size(650, 450);
+            MaximumSize = new Size(650, 450);
         }
 
+        Crypto crypto = new Crypto();
         UnicodeEncoding ByteConverter = new UnicodeEncoding();
-        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
-        MD5CryptoServiceProvider MD5 = new MD5CryptoServiceProvider();
         byte[] encryptedText = { 0 };
         bool rsaActive = true;
-        bool md5Active = true;
-
-        static public byte[] RSAencryption(byte[] data, RSAParameters RSAKey, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] encryptedData = { 0 };
-                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-                    RSA.ImportParameters(RSAKey);
-                    encryptedData = RSA.Encrypt(data, DoOAEPPadding);
-                }
-                return encryptedData;
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
-            }
-        }
-
-        static public byte[] RSAdecryption(byte[] data, RSAParameters RSAKey, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] decryptedData = { 0 };
-                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-                    RSA.ImportParameters(RSAKey);
-                    decryptedData = RSA.Decrypt(data, DoOAEPPadding);
-                }
-                return decryptedData;
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine(e.ToString());
-                return null;
-            }
-        }
-
-        static public byte[] MD5encryption(byte[] data, char[] privateKey)
-        {
-            using (var md5 = new MD5CryptoServiceProvider())
-            {
-                using (var tdes = new TripleDESCryptoServiceProvider())
-                {
-                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(privateKey));
-                    tdes.Mode = CipherMode.ECB;
-                    tdes.Padding = PaddingMode.PKCS7;
-
-                    using (var transform = tdes.CreateEncryptor())
-                    {
-                        return transform.TransformFinalBlock(data, 0, data.Length); 
-                    }
-                }
-            }
-        }
-
-        static public byte[] MD5decryption(byte[] data, char[] privateKey)
-        {
-            using (var md5 = new MD5CryptoServiceProvider())
-            {
-                using (var tdes = new TripleDESCryptoServiceProvider())
-                {
-                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(privateKey));
-                    tdes.Mode = CipherMode.ECB;
-                    tdes.Padding = PaddingMode.PKCS7;
-
-                    using (var transform = tdes.CreateDecryptor())
-                    {
-                        return transform.TransformFinalBlock(data, 0, data.Length);
-                    }
-                }
-            }
-        }
+        bool md5Active = false;
 
         private void rsaButton_Click(object sender, EventArgs e)
         {
@@ -119,12 +39,12 @@ namespace crypto
             byte[] plainText = ByteConverter.GetBytes(txtForEncryption.Text);
             if (rsaActive)
             {
-                encryptedText = RSAencryption(plainText, RSA.ExportParameters(false), false);
+                encryptedText = crypto.RSAencryption(plainText, crypto.RSA.ExportParameters(false), false);
             }
             else if (md5Active)
             {
                 char[] key = txtPrivateKey.Text.ToCharArray();
-                encryptedText = MD5encryption(plainText, key);
+                encryptedText = crypto.MD5encryption(plainText, key);
             }
             txtEncrypted.Text = ByteConverter.GetString(encryptedText);
         }
@@ -134,12 +54,12 @@ namespace crypto
             byte[] decryptedText = { 0 };
             if (rsaActive) 
             {
-                decryptedText = RSAdecryption(encryptedText, RSA.ExportParameters(true), false);
+                decryptedText = crypto.RSAdecryption(encryptedText, crypto.RSA.ExportParameters(true), false);
             }
             else if (md5Active)
             {
                 char[] key = txtPrivateKey.Text.ToCharArray();
-                decryptedText = MD5decryption(encryptedText, key);
+                decryptedText = crypto.MD5decryption(encryptedText, key);
             }
             txtDecrypted.Text = ByteConverter.GetString(decryptedText);
         }
